33a0a5a053d31dda4184cbb3e177c6f6bd1c9530
koudo hinako <b1916571@planet.kanazawa-it.ac.jp>
2019-09-24 19:17:36 +0900

    防御側のスクリプト作成

---
+++ VR_multiPlay_action/Assets/defender/EnemyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float time;
+    public bool collide;
+    
+    void Start()
+    {
+        time = 0f;
+        collide = false;
+    }
+
+    //当たった瞬間呼び出される関数
+    void OnCollisionEnter(Collision collision)
+    {
+        collide = true;
+        //重力の追加
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void Update()
+    {
+        if (collide)
+        {
+            //タイムカウント
+            time += Time.deltaTime;
+            if (time >= 2f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }        
+}
---
+++ VR_multiPlay_action/Assets/defender/PlayerScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScript : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
+    }
+
+}
