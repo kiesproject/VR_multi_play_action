f9c7c93dff14e43b1ec8b80c457501b41d221d38
koudo hinako <b1916571@planet.kanazawa-it.ac.jp>
2019-10-01 17:31:56 +0900

    hitcountの追加

--- VR_multiPlay_action/Assets/defender/PlayerScript.cs
+++ VR_multiPlay_action/Assets/defender/PlayerScript.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerScript : MonoBehaviour
 {
     public AudioSource audioSource;
+    public float hitcount = 0;
 
     private void OnCollisionEnter(Collision collision)
     {
         audioSource.Play();
+        hitcount += 1;
     }
 
 }
